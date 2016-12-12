f1d5c67532cc8bae6ceb1bce91de84341eb153c7
wxf <55121593@qq.com>
2016-12-12 13:50:55 +0800

    test

--- decorationweb/wsgi.py
+++ decorationweb/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+from os.path import join,dirname,abspath
+import sys
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+sys.path.insert(0,PROJECT_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "decorationweb.settings")
 
 application = get_wsgi_application()
